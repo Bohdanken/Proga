4b33cedc24a3d7d3971e0b8716828019453100b1
Батька програміст <shevchenkobogdan2004@gmail.com>
2023-04-23 12:21:36 -0400

    Code with problem

--- app.py
+++ app.py
@@ -20,8 +20,9 @@ import csv
 from run import remove_names_and_date, merge_weather_isw_region
 
 app = Flask(__name__)
-
-
+time=datetime.now()
+path_to_forecasts="./data/forecasts/"
+time_as_path=''
 @app.route('/')
 def index():
     return render_template('home.html')
@@ -59,51 +60,66 @@ def calculate():
 
     query = datetime.strptime(date, '%Y-%m-%d')
 
-    time = request.form['time']
-    time_query = datetime.strptime(time, "%H:%M")
+    time1 = request.form['time']
+    time_query = datetime.strptime(time1, "%H:%M")
 
     chosen_date = datetime.combine(query.date(), time_query.time())
 
     region = request.form['region']
 
     if region == 'All':
+       # if (datetime.now() - time) < timedelta(hours=12):
+        #    get_forecast()
+        weather_forecast_df_all_regions = pd.read_json("./data/forecasts/2023-04-23 5.json")#time_as_path)
+        schedules = {}
         df_regions = pd.read_csv(
-            "data"+sep+"weather_alarms_regions"+sep+"regions.csv", sep=",")
+            "data" + sep + "weather_alarms_regions" + sep + "regions.csv", sep=",")
+        groups = weather_forecast_df_all_regions.groupby(weather_forecast_df_all_regions.index // 24)
+        for index, forecast in groups:
+            city =forecast["city_address"].unique()[0].split(",")[0]
+            index = df_regions.index[df_regions["center_city_en"] == city].tolist()[0]
+            region_df = df_regions.iloc[index]
+            cutoff_time = datetime.now() + timedelta(hours=12)
+            forecast["day_datetime"] = pd.to_datetime(forecast["day_datetime"])
+            filter1=forecast["day_datetime"] <= cutoff_time
+            filter2=forecast['day_datetime'] >= datetime.now()
+            filter=filter1 & filter2
+            filtered_forecast = forecast[filter]
+            filtered_forecast.reset_index(drop=True)
+            filtered_forecast["city"] = filtered_forecast["city_resolvedAddress"].apply(lambda x: x.split(",")[0])
+            filtered_forecast["city"] = filtered_forecast["city"].replace("Хмельницька область", "Хмельницький")
 
-        regions = df_regions['center_city_en'].tolist()
+            text_df = utils.get_text_df(time - timedelta(days=1))
+            text_df["date"] = time.date() - timedelta(days=1)
 
-        # code to change
-        import random
-        predictions = [[] for _ in range(25)]
-        for i in range(25):
-            for j in range(24):
-                predictions[i].append(random.randint(0, 1))
-        # end of code to change
+            input_df = merge_weather_isw_region(filtered_forecast, text_df, pd.DataFrame(region_df).transpose())
 
-        start_time = time_query
+            with open("./model/Series model/best_RandomForestClassifier_V.pkl", "rb") as modelfile:
+                clf = pickle.load(modelfile, encoding="utf-8")
+                schedule = clf.predict(input_df)
+                schedules[city] = schedule
 
         time_array = []
 
-        for i in range(24):
-            time_str = datetime.strftime(
-                start_time + timedelta(hours=i), '%H:00')
+        for i in range(12):
+            time_str = datetime.strftime(time + timedelta(hours=i), '%H:00')
             time_array.append(time_str)
 
         # save predictions to file
         with open("data/predictions/predictions.pkl", "wb") as f:
-            pickle.dump(predictions, f)
+            pickle.dump(schedules, f)
 
-        with open("data/predictions/chosen_date.pkl", "wb") as f:
-            pickle.dump(chosen_date, f)
+        with open("data/predictions/time.pkl", "wb") as f:
+            pickle.dump(time, f)
 
         with open("data/predictions/time_array.pkl", "wb") as f:
             pickle.dump(time_array, f)
 
         with open("data/predictions/regions.pkl", "wb") as f:
-            pickle.dump(regions, f)
+            pickle.dump(weather_forecast_df_all_regions, f)
 
-        return render_template('result_all.html', chosen_date=chosen_date, regions=regions,
-                               predictions=predictions, time_array=time_array, region_idx=0)
+        return render_template('result_all.html', chosen_date=chosen_date, regions=forecast["center_city_en"].unique,
+                               predictions=schedules, time_array=time_array, region_idx=0)
     else:
         city = region
         df_regions = pd.read_csv(
@@ -136,7 +152,7 @@ def calculate():
 
         time_array = []
 
-        for i in range(24):
+        for i in range(12):
             time_str = datetime.strftime(
                 start_time + timedelta(hours=i), '%H:00')
             time_array.append(time_str)
@@ -145,5 +161,22 @@ def calculate():
                                schedule=schedule, time_array=time_array)
 
 
+@app.route('/update', methods=['POST'])
+def get_forecast_for_all():
+    df_regions = pd.read_csv("data"+sep+"weather_alarms_regions"+sep+"regions.csv", sep=",")
+    df_forecast=pd.DataFrame()
+    chosen_date=datetime.now()
+    for i, region in enumerate(df_regions["center_city_en"]):
+        weather_forecast_df = weather.vectorize(weather.get_weather_forecast(chosen_date.isoformat(), region+",UA"))
+        df_forecast=pd.concat([df_forecast,weather_forecast_df],axis=0)
+    df_forecast=df_forecast.reset_index(drop=True)
+    print(len(df_forecast["city_resolvedAddress"].unique()))
+    global time
+    time = chosen_date
+    global time_as_path
+    time_as_path =path_to_forecasts+str(str(chosen_date.date())+" "+str(chosen_date.hour)+".json",)
+    df_forecast.to_json(path_or_buf=time_as_path)
+
 if __name__ == '__main__':
+    #get_forecast()
     app.run(debug=True)
